c369bc542358f2bfc9fb7ffe29b3286cf79fdde9
Yash Sharma <yash3498@gmail.com>
2018-07-10 17:09:25 +0530

    added capability to append new data to the old dataset

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,23 +1,19 @@
 import pandas as pd
 import re
 import string
+import os
 
 def processSynopsis(X):
     # Convert the Synopsis to all lower characters
     X = X.lower()
-
     # Replace [Written by MAL Rewrite] wiht ``
     cleaned = X.replace("[written by mal rewrite]", "")
-
     # Remove all the puncation
     cleaned = re.sub(r'[,\.!"?-]', '', cleaned)
-
     # Remove extra characters
     cleaned = re.sub(r'[&#039;]', '', cleaned)
-
     # Remove extra white spaces
     cleaned = cleaned.strip()
-
     # Replace newline with ``
     cleaned = cleaned.replace("\n", " ")
 
@@ -25,13 +21,10 @@ def processSynopsis(X):
     return cleaned
 
 def processSynopsis_v2(X):
-
     # Replace [Written by MAL Rewrite] wiht ``
     cleaned = X.replace("[Written by MAL Rewrite]", "")
-
     # Remove extra characters
     cleaned = re.sub(r'[&#039;]', '', cleaned)
-
     # Remove extra white spaces
     cleaned = cleaned.strip()
 
@@ -41,10 +34,8 @@ def processSynopsis_v2(X):
 def processRatings(X):
     # Convert the Synopsis to all lower characters
     X = X.lower()
-
     # Remove all the puncation
     cleaned = re.sub(r'[,\.!"()?-]', '', X)
-
     # Remove extra white spaces
     cleaned = cleaned.strip()
 
@@ -54,21 +45,37 @@ def processRatings(X):
 def processTitle(X):
     # Remove extra characters
     cleaned = re.sub(r'[&#039;]', '', X)
-
     # Remove extra characters
     cleaned = cleaned.replace(";", ":")
-
     # Remove extra white spaces
     cleaned = cleaned.strip()
 
     # return the Cleaned Version
     return cleaned
 
-anime = pd.read_csv('Anime.csv')
+# open the csv file
+anime = pd.read_csv('anime.csv')
+
+# Apply Pre-Processing Steps
 anime['Title'] = anime.Title.apply(processTitle)
+anime['Synopsis'] = anime.Synopsis.apply(processSynopsis_v2)
+
+# New Columns 'c' stands for cleaned
 anime['cSynopsis'] = anime.Synopsis.apply(processSynopsis)
 anime['cGenre'] = anime.Genre.str.lower()
 anime['cRating'] = anime.Rating.apply(processRatings)
-anime['Synopsis'] = anime.Synopsis.apply(processSynopsis_v2)
 
-anime.to_csv('cleaned_anime_data.csv', index = False)
+# Check dataset already exists on the disk
+if os.path.isfile('cleaned_anime_data.csv'):
+    # open the dataset
+    df = pd.read_csv('cleaned_anime_data.csv')
+    # append the new dataset to the old one
+    df = df.append(anime)
+    # for safety check remove all duplicates
+    df.drop_duplicates(subset = ["Anime_ID"], inplace = True)
+    # save the dataset back to the disk
+    df.to_csv('cleaned_anime_data.csv', index = False)
+
+# else save the new dataset
+else:
+    anime.to_csv('cleaned_anime_data.csv', index = False)
